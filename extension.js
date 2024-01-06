e0faca7f472d0e18e4c6738ba0d7ebb6c54598ba
donchuru <onchuru@ualberta.ca>
2024-01-06 14:48:14 -0700

    extension now fetches code

--- extension.js
+++ extension.js
@@ -20,14 +20,47 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('comment-assistant.commentCode', function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Comments complete!');
+			// check if we have an active workspace
+			if (vscode.workspace.workspaceFolders) {
+				// Get all open text documents in the workspace
+				const openDocuments = vscode.workspace.textDocuments;
+							
+				if (openDocuments.length > 0) {
+					// Extract file paths from the open documents
+					const filePaths = openDocuments.map((document) => document.uri.fsPath);
+			
+					// Show a quick pick dropdown with file paths
+					vscode.window.showQuickPick(filePaths, {
+						placeHolder: 'Select a file to open'
+					}).then((selectedPath) => {
+						if (selectedPath) {
+							// Open the text document
+							vscode.workspace.openTextDocument(selectedPath).then((document) => {
+								// Get the text content
+								const textContent = document.getText();
+								
+								// Do something with the text content
+								console.log('Text content:', textContent);
+							}).catch((error) => {
+								console.error('Error opening the document:', error);
+							});
+						}
+					});
+				} else {
+					vscode.window.showInformationMessage('No open documents in the workspace.');
+				}
+
+			} else {
+				// No active workspace
+				vscode.window.showErrorMessage('No workspace opened.');
+			}
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+// This method is called when your extension is deactivated- just to clean everything up at the end
 function deactivate() {}
 
 module.exports = {
