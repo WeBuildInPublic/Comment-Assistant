46718b78871f9d94d8a45ea1ba8307eb1786aaea
Isaac Joffe <isaac11joffe@gmail.com>
2024-01-06 17:09:55 -0700

    works?

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
-# # import os
-# # import openai
-# # import sys
+import os
+import openai
+import sys
 
 
-# # API_KEY = "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"
-# # PROMPT = """Throughly yet concisely comment and document the following code, following all conventions of the programming language. Include standard documentation headers and inline comments where appriopriate. Reply only with the new code and comments, and do not change the actual code at all.
+API_KEY = ""
+PROMPT = """Throughly yet concisely comment and document the following code, following all conventions of the programming language. Include standard documentation headers and inline comments where appriopriate. Reply only with the new code and comments, and do not change the actual code at all.
 
-# # """
-
-
-# # def main():
-# #     # print("Hello, World")
-# #     # inputCode = sys.argv[1]
-# #     # outputCode = ""
-# #     # try:
-# #     #     openai.api_key = API_KEY
-# #     #     response = openai.Completion.create(
-# #     #         model="text-davinci-003",
-# #     #         prompt=PROMPT+inputCode,
-# #     #         max_tokens=256,
-# #     #         n=1,
-# #     #         temperature=0.5,
-# #     #     )
-# #     #     outputCode = response["choices"][0]["text"]
-# #     # except:
-# #     #     outputCode = "ERROR"
-
-# #     # print(outputCode)
-# #     print("Hello, World")
-# #     # sys.stdout.flush()
+"""
 
 
-# # if __name__ == "__main__":
-# #     main()
+inputCode = sys.argv[1]
+outputCode = ""
+try:
+    openai.api_key = API_KEY
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=PROMPT+inputCode,
+        max_tokens=512,
+        n=1,
+        temperature=0.5,
+    )
+    outputCode = response["choices"][0]["text"]
+except:
+    outputCode = "ERROR"
 
-# main.py
-print("Python script execution completed successfully.")
-
-# import random, time
-# while True:
-#     time.sleep(random.random() * 5)  # wait 0 to 5 seconds
-#     temperature = (random.random() * 20) - 5  # -5 to 15
-#     print(temperature, flush=True, end='')
+print(outputCode, flush=True, end="")
